c0e621a779403b863d7be52f0cb1ad14d1651cdc
nvu <khoinguyen.works@gmail.com>
2024-10-26 14:54:46 +0700

    Restart

--- README.md
+++
@@ -1,3 +0,0 @@
-# Welcome to my website
-
-Here is the URL: https://khoiinguyen.com
---
+++ asset/js/script.js
@@ -0,0 +1,6 @@
+// Example JavaScript for additional interactivity
+document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Direct you nowww!');
+    });
+});
